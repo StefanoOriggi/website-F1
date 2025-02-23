e33cbbc60d78c165605d1aa35bf762f4fe355798
unknown <stefano.origgi@issgreppi.it>
2025-02-23 15:16:19 +0100

    Modifica Program.cs

--- Progetto/Program.cs
+++ Progetto/Program.cs
@@ -1,12 +1,27 @@
-using Progetto.Models;
 using Progetto.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("FilmAPIConnection");
-var serverVersion = ServerVersion.AutoDetect(connectionString);
 
-//Aggiungiamo il servizio database tramite EF Core con MariaDB
+
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddOpenApiDocument(config =>
+    {
+        config.Title = "Formula-API";
+        config.DocumentName = "Formula-API";
+        config.Version = "v1";
+    }
+);
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+}
+
+var connectionString = builder.Configuration.GetConnectionString("FormulaAPIConnection");
+var serverVersion = ServerVersion.AutoDetect(connectionString);
 builder.Services.AddDbContext<FormulaContext>(
     opt => opt.UseMySql(connectionString, serverVersion)
     .LogTo(Console.WriteLine, LogLevel.Information)
@@ -15,33 +30,41 @@ builder.Services.AddDbContext<FormulaContext>(
     );
 
 var app = builder.Build();
+
 if (app.Environment.IsDevelopment())
 {
-    //permette di ottenere una rotta dove vedere la documentazione delle API secondo lo standard OpenAPI
     app.MapOpenApi();
-    //permette a Swagger (NSwag) di generare un file JSON con le specifiche delle API
     app.UseOpenApi();
-    //permette di configurare l'interfaccia SwaggerUI (l'interfaccia grafica web di Swagger (NSwag) che permette di interagire con le API)
     app.UseSwaggerUi(config =>
     {
-        config.DocumentTitle = "Formula API v1";
+        config.DocumentTitle = "FormulaApi";
         config.Path = "/swagger";
         config.DocumentPath = "/swagger/{documentName}/swagger.json";
         config.DocExpansion = "list";
     });
 }
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
+
 app.UseHttpsRedirection();
-// Configure Swagger UI before static files
-app.UseSwaggerUi(config =>
+
+app.UseDefaultFiles(new DefaultFilesOptions
 {
-    config.DocumentTitle = "Film API v1";
-    config.Path = "/swagger";
-    config.DocumentPath = "/swagger/{documentName}/swagger.json";
-    config.DocExpansion = "list";
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(builder.Environment.WebRootPath, "pages")
+    ),
+    RequestPath = ""
 });
+
 app.UseStaticFiles();
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(builder.Environment.WebRootPath, "pages")
+    )
+});
+app.MapGet("/", () =>
+{
+    return Results.Redirect("homepage.html");
+});
+
 app.Run();
